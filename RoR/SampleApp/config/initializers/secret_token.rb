3d6e76dd7200efbba1daef56c21d198e49ef71a0
MinhMinh <minhdanghuu@gmail.com>
2013-11-04 18:21:18 -0800

    rails 4.0.1

---
+++ RoR/SampleApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '6e1e6ecd1a82d9d082ba4335c2df1ed0235a7bb1951fc99d2136d845a27c1944a248e83b000e2cda7614dc4eefd60e515cade73848140e498637d036ecfcd47f'
--- RoR/VTS/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-VTS::Application.config.secret_token = 'a44a0c7db0e2a7b0512f45191f21a20ee5c1e31378376c19154c06c81af93f1798925c62634c0a8bbe03ccc9998f20bc453929260e8aeb5c09ccda57227609e5'
